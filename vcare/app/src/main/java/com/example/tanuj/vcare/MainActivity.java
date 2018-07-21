d9b0f413d5aee2afe5c9e1615866dca39e8e5631
tanuj <tanuj.bohra@workindia.in>
2018-07-22 01:43:00 +0530

    Payment API integration

--- vcare/app/src/main/java/com/example/tanuj/vcare/MainActivity.java
+++ vcare/app/src/main/java/com/example/tanuj/vcare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tanuj.vcare;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +8,18 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PaymentResultListener {
+
+    Button mPayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, MainOptions.class);
             startActivity(intent);
         }
+        mPayButton = (Button)findViewById(R.id.button_pay);
+        mPayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPayment();
+            }
+        });
     }
     public void onSignOut(){
         new User(MainActivity.this).removeUser();
@@ -44,5 +62,40 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    private void startPayment() {
+        int amount = 10; //amount to deduce
 
+        Checkout checkout = new Checkout();
+
+        //set app logo here
+        checkout.setImage(R.mipmap.ic_launcher);
+
+        final Activity activity = this;
+
+
+        //Pass your payment options to the Razorpay Checkout as a JSONObject
+        try {
+            JSONObject options = new JSONObject();
+
+            //Use the same name fields
+            options.put("name", "Merchant Name");
+            options.put("description", "Order #123456");
+            options.put("currency", "INR");
+            options.put("amount", amount * 100);
+            checkout.open(activity, options);
+        } catch(Exception e) {
+            Log.e("PAY", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(MainActivity.this,"Payment Successful",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(MainActivity.this,"Payment Failure",Toast.LENGTH_LONG).show();
+
+    }
 }
